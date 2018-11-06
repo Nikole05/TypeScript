bdc752c683ba325401c279dd46bc083fe74e2e28
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-06 17:44:32 +0200

    strictNullChecks Option

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,15 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string ;
+    let playerName: string = 'Audrey';
     logPlayer(playerName);
 
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+    let messagesElement: HTMLElement | null = document.getElementById('messages');
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
     console.log('Starting new game.');
 }
 
 function logPlayer(name) {
     console.log(`New game starting for player: ${name}`);
 }
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
