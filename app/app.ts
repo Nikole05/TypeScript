6fd039faf3aa1f4b59fd0a3dc680d0519419215e
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-06 17:28:15 +0200

    type annotations

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,15 @@
 function startGame() {
     // starting a new game
+
+    const playerName: string = 'Audrey';
+    logPlayer(playerName);
+
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
     console.log('Starting new game.');
 }
 
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
 document.getElementById('startGame').addEventListener('click', startGame);
