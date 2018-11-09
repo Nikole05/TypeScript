4f0decad4e6e77ae8d79a73192f0a5c9a3a80739
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-09 23:28:47 +0200

    adding type annotations and default parameter valuer

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,31 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string = 'Audrey';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    let messagesElement: HTMLElement | null = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
-    console.log('Starting new game.');
+    postScore(100, playerName);
+    
 }
 
-function logPlayer(name) {
+function logPlayer(name:string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
 }
+
+function postScore(score: number, playerName?: string): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+function getInputValue(elementID: string): string | undefined {
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === ''){
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
